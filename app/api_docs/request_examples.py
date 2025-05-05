204b76786017d37a79866e06e0a9821e3d6ab00b
Artyom Loginov <loginov.artem@phystech.edu>
2025-05-05 17:11:22 +0300

    отредактировал примеры

--- app/api_docs/request_examples.py
+++ app/api_docs/request_examples.py
@@ -1,4 +1,34 @@
 from fastapi import Body
+from datetime import datetime, timedelta, date
+
+example_create_project = Body(
+    openapi_examples={
+        "normal": {
+            "summary": "Типовой запрос",
+            "description": "Типовой запрос для создания проекта",
+            "value": {
+                "name": "Презентация",
+                "description": "Подготовить презентацию для доклада на конференцию МФТИ"
+            }
+        },
+        "empty_description": {
+            "summary": "Запрос без описания",
+            "description": "Запрос для создания проекта без указания "
+                           "описания. В поле `description` будет "
+                           "автоматически подставлено значение `None`",
+            "value": {
+                "name": "Проект без описания"
+            },
+        },
+        "invalid": {
+            "summary": "Некорректные данные, возвращается ошибка 422",
+            "value": {
+                "name": 123,
+                "description": 456
+            },
+        },
+    }
+)
 
 
 example_create_task = Body(
@@ -7,9 +37,10 @@ example_create_task = Body(
             "summary": "Типовой запрос",
             "description": "Типовой запрос для создания задачи",
             "value": {
-                "task_description": "Подготовить презентацию",
-                "assignee": "Василий",
-                "due_date": "2025-01-20"
+                "description": "Подготовить план",
+                "assignee": "Иван Иванов",
+                "due_date": (date.today() + timedelta(days=7)).strftime("%Y-%m-%d"),
+                "project": 1
             }
         },
         "empty_date": {
@@ -18,16 +49,37 @@ example_create_task = Body(
                            "крайнего срока исполнения. В поле `due_date` "
                            "будет автоматически подставлен завтрашний день",
             "value": {
-                "task_description": "Подготовить презентацию",
-                "assignee": "Василий"
+                "description": "Отрепетировать выступление",
+                "assignee": "Иван Иванов",
+                "project": 1
             },
         },
-        "invalid": {
-            "summary": "Некорректные данные, возвращается ошибка 422",
+        "empty_project": {
+            "summary": "Запрос без указания проекта",
+            "description": "Запрос для создания задачи без указания "
+                           "ID проекта. В поле `project` будет "
+                           "автоматически подставлено значение `None`",
             "value": {
-                "task_description": "Подготовить презентацию",
+                "description": "Пообедать",
+                "assignee": "Иван Иванов",
+                "due_date": (date.today() + timedelta(days=1)).strftime("%Y-%m-%d")
+            },
+        },
+        "invalid_data": {
+            "summary": "Некорректная дата, возвращается ошибка 422",
+            "value": {
+                "description": "Подготовить презентацию",
                 "assignee": "Василий",
                 "due_date": "сегодня"
+            },
+        },
+        "invalid_assignee": {
+            "summary": "Некорректный исполнитель, возвращается ошибка 422",
+            "description": "Запрос для создания задачи с несуществующим исполнителем.",
+            "value": {
+                "description": "Подготовить план",
+                "assignee": "Иван",
+                "due_date": (date.today() + timedelta(days=1)).strftime("%Y-%m-%d")
             },
         },
     }
