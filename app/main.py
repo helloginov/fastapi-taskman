803fe82eba680a5c8389ffc310e507e0220fddea
Artyom Loginov <loginov.artem@phystech.edu>
2025-05-05 17:27:57 +0300

    Ренейминг, удаление неактуального кода

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.routes import (task, task_v2, utils, async_routes, auth)
+from app.routes import (task, utils, async_routes, auth)
+from app.routes import task
 # from contextlib import asynccontextmanager  # Uncomment if you need to create tables on app start >>>
 # from app.db import init_database
 
@@ -27,7 +28,7 @@ app = FastAPI(
 )
 
 # app.include_router(task.router)
-app.include_router(task_v2.router)
+app.include_router(task.router)
 app.include_router(utils.router)
 app.include_router(async_routes.router)
 app.include_router(auth.router)
--- app/routes/task.py
+++ app/routes/task.py
@@ -1,76 +1,143 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlmodel import Session, select
+from app.db import get_session
 from ..schemas import task as schema_task
-from typing import List, Annotated
-from app.data_handler import (write_task_to_csv, read_tasks_from_csv,
-                              read_task_from_csv, update_task_in_csv)
+from typing import Annotated, List
 from ..api_docs import request_examples
 
-router = APIRouter(prefix="/v1/tasks", tags=["Управление задачами в файле"])
+router = APIRouter(prefix="/v2/tasks", tags=["Управление задачами в БД"])
+
+@router.post("/new_project", status_code=status.HTTP_201_CREATED, response_model=schema_task.ProjectRead,
+             summary='Добавить проект')
+def create_project(
+    project: Annotated[schema_task.ProjectCreate, request_examples.example_create_project],
+    session: Session = Depends(get_session)
+):
+    new_project = schema_task.Project(
+        name=project.name,
+        description=project.description
+    )
+    session.add(new_project)
+    session.commit()
+    session.refresh(new_project)
+    return new_project
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED,
-             response_model=schema_task.TaskRead,
+@router.get("/projects", status_code=status.HTTP_200_OK, response_model=List[schema_task.ProjectRead],
+            summary='Получить список всех проектов.')
+def read_projects(session: Session = Depends(get_session)):
+
+    projects = session.exec(select(schema_task.Project)).all()
+    if not projects:
+        raise HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT,
+            detail="No projects found."
+        )
+    return projects
+
+
+@router.post("/new_task", status_code=status.HTTP_201_CREATED, response_model=schema_task.TaskRead,
              summary = 'Добавить задачу')
-def create_task(task: Annotated[
-                        schema_task.TaskCreate,
-                        request_examples.example_create_task
-    ]):
-    """
-    Добавить задачу.
-    """
-    new_task = write_task_to_csv(task)
+def create_task(
+    task: Annotated[schema_task.TaskCreate, request_examples.example_create_task],
+    session: Session = Depends(get_session)
+):
+    statement = select(schema_task.User).where(schema_task.User.name == task.assignee)
+    existing_user = session.exec(statement).first()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"{task.assignee} not found"
+        )
+    new_task = schema_task.Task(
+        description=task.description,
+        assignee=existing_user.id,
+        due_date=task.due_date,
+        project=task.project
+    )
+    session.add(new_task)
+    session.commit()
+    session.refresh(new_task)
     return new_task
 
 
-@router.get("/", status_code=status.HTTP_200_OK, response_model=List[schema_task.TaskRead])
-def read_tasks():
-    tasks = read_tasks_from_csv()
-    if tasks is None:
+
+@router.get("/project/{project_id}/tasks", status_code=status.HTTP_200_OK, response_model=List[schema_task.TaskRead],
+            summary='Получить все задачи, связанные с определенным проектом')
+def read_tasks_by_project(project_id: int, session: Session = Depends(get_session)):
+
+    tasks = session.exec(select(schema_task.Task).where(schema_task.Task.project == project_id)).all()
+    if not tasks:
         raise HTTPException(
             status_code=status.HTTP_204_NO_CONTENT,
-            detail=f"The task list is empty."
+            detail=f"No tasks found for project ID {project_id}."
         )
     return tasks
 
 
-@router.get("/{task_id}", response_model=schema_task.TaskRead)
-def read_task_by_id(task_id: int):
-    task = read_task_from_csv(task_id)
+@router.get("/tasks/no_project", status_code=status.HTTP_200_OK, response_model=List[schema_task.TaskRead],
+            summary='Получить все задачи, которые не связаны с каким-либо проектом')
+def read_tasks_without_project(session: Session = Depends(get_session)):
+
+    tasks = session.exec(select(schema_task.Task).where(schema_task.Task.project == None)).all()
+    if not tasks:
+        raise HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT,
+            detail="No tasks found without a project."
+        )
+    return tasks
+
+
+@router.get("/user/{user_id}/tasks", status_code=status.HTTP_200_OK, response_model=List[schema_task.TaskRead],
+            summary='Получить все задачи, связанные с определенным пользователем')
+def read_tasks_by_user(user_id: int, session: Session = Depends(get_session)):
+
+    tasks = session.exec(select(schema_task.Task).where(schema_task.Task.assignee == user_id)).all()
+    if not tasks:
+        raise HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT,
+            detail=f"No tasks found for user ID {user_id}."
+        )
+    return tasks
+
+
+@router.patch("/{task_id}", status_code=status.HTTP_200_OK, response_model=schema_task.TaskRead,
+              summary='Обновить задачу по ID')
+def update_task_by_id(task_id: int, data_for_update: dict, session: Session = Depends(get_session)):
+
+    statement = select(schema_task.Task).where(schema_task.Task.id == task_id)
+    task = session.exec(statement).first()
 
     if not task:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Task with ID {task_id} not found"
+            detail=f"Task with ID {task_id} not found."
         )
 
+    for key, value in data_for_update.items():
+        if hasattr(task, key):
+            setattr(task, key, value)
+
+    session.add(task)
+    session.commit()
+    session.refresh(task)
     return task
 
 
-@router.patch("/{task_id}", status_code=status.HTTP_200_OK, response_model=schema_task.TaskRead)
-def update_task_by_id(task_id: int, data_for_update: dict):
-    task_fields = set(schema_task.TaskCreate.model_fields.keys())
+@router.delete("/{task_id}", status_code=status.HTTP_200_OK, response_model=dict,
+              summary='Удалить задачу по ID.')
+def delete_task_by_id(task_id: int, session: Session = Depends(get_session)):
 
-    if not set(data_for_update.keys()) <= task_fields:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"An update request must only contain one or more of the following fields: {', '.join(task_fields)}."
-        )
-
-    task = update_task_in_csv(task_id, data_for_update)
+    statement = select(schema_task.Task).where(schema_task.Task.id == task_id)
+    task = session.exec(statement).first()
 
     if not task:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Task with ID {task_id} not found"
+            detail=f"Task with ID {task_id} not found."
         )
 
-    return task
-
-
-@router.delete("/{task_id}", status_code=status.HTTP_200_OK)
-def delete_task_by_id(task_id: int):
-    raise HTTPException(
-        status_code=status.HTTP_501_NOT_IMPLEMENTED,
-        detail=f"Unable to delete task with ID {task_id}: "
-               f"method is not implemented yet."
-    )
+    session.delete(task)
+    session.commit()
+    return {'deleted task': task}
