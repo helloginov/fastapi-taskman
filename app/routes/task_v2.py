f561da098bdbbea80463b25773c0643050b07026
Artyom Loginov <loginov.artem@phystech.edu>
2025-05-05 17:26:17 +0300

    Наладил обработку простых CRUD запросов

--- app/routes/task_v2.py
+++ app/routes/task_v2.py
@@ -64,7 +64,7 @@ def create_task(
 
 
 @router.get("/project/{project_id}/tasks", status_code=status.HTTP_200_OK, response_model=List[schema_task.TaskRead],
-            summary='Получить все задачи, связанные с определенным проектом.')
+            summary='Получить все задачи, связанные с определенным проектом')
 def read_tasks_by_project(project_id: int, session: Session = Depends(get_session)):
 
     tasks = session.exec(select(schema_task.Task).where(schema_task.Task.project == project_id)).all()
@@ -77,7 +77,7 @@ def read_tasks_by_project(project_id: int, session: Session = Depends(get_sessio
 
 
 @router.get("/tasks/no_project", status_code=status.HTTP_200_OK, response_model=List[schema_task.TaskRead],
-            summary='Получить все задачи, которые не связаны с каким-либо проектом.')
+            summary='Получить все задачи, которые не связаны с каким-либо проектом')
 def read_tasks_without_project(session: Session = Depends(get_session)):
 
     tasks = session.exec(select(schema_task.Task).where(schema_task.Task.project == None)).all()
@@ -90,7 +90,7 @@ def read_tasks_without_project(session: Session = Depends(get_session)):
 
 
 @router.get("/user/{user_id}/tasks", status_code=status.HTTP_200_OK, response_model=List[schema_task.TaskRead],
-            summary='Получить все задачи, связанные с определенным пользователем.')
+            summary='Получить все задачи, связанные с определенным пользователем')
 def read_tasks_by_user(user_id: int, session: Session = Depends(get_session)):
 
     tasks = session.exec(select(schema_task.Task).where(schema_task.Task.assignee == user_id)).all()
@@ -103,7 +103,7 @@ def read_tasks_by_user(user_id: int, session: Session = Depends(get_session)):
 
 
 @router.patch("/{task_id}", status_code=status.HTTP_200_OK, response_model=schema_task.TaskRead,
-              description='Обновить задачу по ID.')
+              summary='Обновить задачу по ID')
 def update_task_by_id(task_id: int, data_for_update: dict, session: Session = Depends(get_session)):
 
     statement = select(schema_task.Task).where(schema_task.Task.id == task_id)
@@ -125,8 +125,8 @@ def update_task_by_id(task_id: int, data_for_update: dict, session: Session = De
     return task
 
 
-@router.delete("/{task_id}", status_code=status.HTTP_200_OK, response_model=schema_task.TaskRead,
-              description='Удалить задачу по ID.')
+@router.delete("/{task_id}", status_code=status.HTTP_200_OK, response_model=dict,
+              summary='Удалить задачу по ID.')
 def delete_task_by_id(task_id: int, session: Session = Depends(get_session)):
 
     statement = select(schema_task.Task).where(schema_task.Task.id == task_id)
